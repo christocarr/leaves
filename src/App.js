90a75d9aa28fd489103cdddca564ba90b54d2c14
christocarr <christocarr@outlook.com>
2020-09-28 11:51:47 +0100

    Feature: home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import Header from './components/Header'
+import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <main>
-      <Header>
-        <Header.Logo />
-      </Header>
-    </main>
-  );
+  return <Home />;
 }
 
 export default App;
