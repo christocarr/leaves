a49bbcf4bb64efc51eb9eb1e9214fa907764ec5b
christocarr <christocarr@outlook.com>
2020-09-29 13:13:40 +0100

    Feature: new page

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <>
+      <Header></Header>
+      <div>About Us</div>
+    </>
+  );
+}
+
+export default Home;
