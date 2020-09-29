52e5c9659736e39d6a8eb5791c0c34f0cf2749ad
christocarr <christocarr@outlook.com>
2020-09-29 12:39:50 +0100

    Add navigation component to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { HeaderContainer, Logo } from "./styles";
+import Navigation from '../Navigation'
 
 function Header({ children, ...restProps }) {
-  return <HeaderContainer>{children}</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      {children}
+      <Navigation />
+    </HeaderContainer>
+  )
 }
 
 Header.Logo = function HeaderLogo({ children, ...restProps }) {
