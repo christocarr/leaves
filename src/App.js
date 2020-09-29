e8dc9a123fbf85e96b27aefc28175bc6f8a67bc6
christocarr <christocarr@outlook.com>
2020-09-29 13:15:14 +0100

    Fix: add exact prop to Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
+        </Route>
+        <Route path="/about_us">
+          <AboutUs />
         </Route>
       </Switch>
     </Router>
