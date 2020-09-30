d83c0a9dcc51f99fcb68aba6cd05bfa01f618b31
christocarr <christocarr@outlook.com>
2020-09-30 09:08:57 +0100

    Feature: add product card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import ProductCard from "../components/ProductCard";
+import { productData } from "../productData";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(productData);
+  }, []);
+
   return (
     <>
       <Header />
-      <ProductCard />
+      {products.map((product) => (
+        <ProductCard key={product.id}>
+          <ProductCard.Title>{product.name}</ProductCard.Title>
+          <ProductCard.Description>{product.desc}</ProductCard.Description>
+        </ProductCard>
+      ))}
     </>
   );
 }
