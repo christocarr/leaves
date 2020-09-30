53bdcc2404fbc41685817d5f0e319391edd12e46
christocarr <christocarr@outlook.com>
2020-09-30 09:08:04 +0100

    Test: mock data

---
+++ src/productData.js
@@ -0,0 +1,26 @@
+export const productData = [
+  {
+    id: 1,
+    name: "Plant One",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
+  },
+  {
+    id: 2,
+    name: "Plant Two",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
+  },
+  {
+    id: 3,
+    name: "Plant Three",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
+  },
+  {
+    id: 4,
+    name: "Plant Four",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
+  },
+];
