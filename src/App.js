1c8aef329bbab41fdfc7415b422e8b833678b01b
christocarr <christocarr@outlook.com>
2020-10-01 10:57:23 +0100

    Add context provider to home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductContext from "./context/productContext";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
+import { productData } from "./productData";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <ProductContext.Provider value={productData}>
+            <Home />
+          </ProductContext.Provider>
         </Route>
         <Route path="/about_us">
           <AboutUs />
