251d2178a0edcbf7c40e0d80fee8b81158b65fd5
christocarr <christocarr@outlook.com>
2020-10-02 16:14:00 +0100

    Add styling to search container git push

--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 90vw;
+  margin: 1em auto 0;
+`;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  padding: 0.5em;
+`;
