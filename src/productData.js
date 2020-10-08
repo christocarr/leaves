668fb1fe91135504fc85e50a8393001573ffaf2c
christocarr <christocarr@outlook.com>
2020-10-08 15:45:03 +0100

    Modify: add popularity property

--- src/productData.js
+++ src/productData.js
@@ -4,41 +4,47 @@ export const productData = [
     name: "Plant One",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 10
+    price: 10,
+    popularity: 1
   },
   {
     id: 2,
     name: "Plant Two",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 13
+    price: 13,
+    popularity: 2
   },
   {
     id: 3,
     name: "Plant Three",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 12
+    price: 12,
+    popularity: 5
   },
   {
     id: 4,
     name: "Plant Four",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 16
+    price: 16,
+    popularity: 4
   },
   {
     id: 5,
     name: "Plant Five",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 12
+    price: 12,
+    popularity: 3
   },
   {
     id: 6,
     name: "Plant Six",
     desc:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati eius cupiditate necessitatibus voluptatibus delectus fugiat, numquam veritatis explicabo molestiae tempora. Culpa incidunt dolorem maxime facilis odio. Facilis perferendis deserunt nobis.",
-    price: 15
+    price: 15,
+    popularity: 2
   },
 ];
