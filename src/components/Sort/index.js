54ff620c7d35e592ad401625ef80396e08775e5b
christocarr <christocarr@outlook.com>
2020-10-08 14:02:04 +0100

    Feature: add product sort component

---
+++ src/components/Sort/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Select } from './styles';
+
+function Sort({ sortSelect, setSortSelect, children, ...restProps }) {
+	return (
+		<Container>
+			<Select
+				value={sortSelect}
+				onChange={({ target }) => setSortSelect(target.value)}
+			>
+        {/* set default sort to most popular */}
+				<option value="priceLowToHigh">Sort by: Price (low to high)</option>
+			</Select>
+		</Container>
+	);
+}
+
+export default Sort;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,39 @@
-import React, { useState, useEffect, useContext } from "react";
-import Header from "../components/Header";
-import Search from "../components/Search";
-import ProductCard from "../components/ProductCard";
-import productContext from "../context/productContext";
+import React, { useState, useEffect, useContext } from 'react';
+import Header from '../components/Header';
+import Search from '../components/Search';
+import Sort from '../components/Sort'
+import ProductCard from '../components/ProductCard';
+import productContext from '../context/productContext';
 
 function Home() {
-  const productsData = useContext(productContext);
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+	const productsData = useContext(productContext);
+	const [searchResults, setSearchResults] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [sortSelect, setSortSelect] = useState('');
 
-  useEffect(() => {
-    const filteredProducts = productsData.filter(({ name }) =>
-      name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(filteredProducts);
-  }, [productsData, searchTerm]);
+	useEffect(() => {
+		const filteredProducts = productsData.filter(({ name }) =>
+			name.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setSearchResults(filteredProducts);
+	}, [productsData, searchTerm]);
 
-  return (
-    <>
-      <Header />
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <ProductCard.Group>
-        {searchResults.map((product) => (
-          <ProductCard key={product.id}>
-            <ProductCard.Title>{product.name}</ProductCard.Title>
-            <ProductCard.Description>{product.desc}</ProductCard.Description>
-            <ProductCard.Price>£{product.price}</ProductCard.Price>
-          </ProductCard>
-        ))}
-      </ProductCard.Group>
-    </>
-  );
+	return (
+		<>
+			<Header />
+      <Sort sortSelect={sortSelect} setSortSelect={setSortSelect} />
+			<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			<ProductCard.Group>
+				{searchResults.map((product) => (
+					<ProductCard key={product.id}>
+						<ProductCard.Title>{product.name}</ProductCard.Title>
+						<ProductCard.Description>{product.desc}</ProductCard.Description>
+						<ProductCard.Price>£{product.price}</ProductCard.Price>
+					</ProductCard>
+				))}
+			</ProductCard.Group>
+		</>
+	);
 }
 
 export default Home;
